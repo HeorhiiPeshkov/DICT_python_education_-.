5e17047e4696196d4c39cf377915f237e05f2565
HeorhiiPeshkov <h.p.pieshkov@student.khai.edu>
2025-03-13 17:05:50 +0200

    Functions added

--- arithmetic_test/arithmetic_test.py
+++ arithmetic_test/arithmetic_test.py
@@ -1,67 +1,78 @@
 import random
 print('Hello user!')
 
+def mode_choosing():
+    while True:
+        mode = input('What difficult mode you want:\nIf you want easy mode type 1\nIf you want hard mode type 2\n>')
 
-while True:
-    mode = input('What difficult mode you want:\nIf you want easy mode type 1\nIf you want hard mode type 2\n>')
-    if mode == '1':
-        operations = ['*', '-', '+']
-        correct_answers = 0
-        for _ in range(5):
-            number_1 = random.randint(2, 9)
-            number_2 = random.randint(2, 9)
-            operation = random.choice(operations)
-            example = f'{number_1}{operation}{number_2}'
-            print(example)
-            right_answer = eval(example)
+        if mode == '1':
+            def mode_one():
+                operations = ['*', '-', '+']
+                correct_answers = 0
+                for _ in range(5):
+                    number_1 = random.randint(2, 9)
+                    number_2 = random.randint(2, 9)
+                    operation = random.choice(operations)
+                    example = f'{number_1}{operation}{number_2}'
+                    print(example)
+                    right_answer = eval(example)
+                    while True:
+                        try:
+                            answer = int(input('>'))
+                            if answer == right_answer:
+                                print('Right!')
+                                correct_answers += 1
+                                break
+                            else:
+                                print('Wrong!')
+                                break
+                        except ValueError:
+                            print("Incorrect parameters")
+                    print(f'Your mark is {correct_answers}/5.')
+                return correct_answers
+            mode_one()
+            break
+
+
+        elif mode == '2':
+                def mode_two():
+                    correct_answers = 0
+                    for _ in range(5):
+                        number = random.randint(11, 29)
+                        example = f'{number}**2'
+                        print(example)
+                        right_answer = number ** 2
+                        while True:
+                            answer = input('>')
+                            if answer.isdigit():
+                                if int(answer) == right_answer:
+                                    print('Right!')
+                                    correct_answers += 1
+                                    break
+                                else:
+                                    print('Wrong!')
+                                    break
+                            else:
+                                print('Incorrect format.')
+                        print(f'Your mark is {correct_answers}/5.')
+                    return correct_answers
+                correct_answers = mode_two()
+        else:
+            print("Invalid mode. Please enter 1 or 2.")
+
+
+        def results():
             while True:
-                try:
-                    answer = int(input('>'))
-                    if answer == right_answer:
-                        print('Right!')
-                        correct_answers += 1
-                        break
-                    else:
-                        print('Wrong!')
-                        break
-                except ValueError:
-                    print("Incorrect parameters")
-            print(f'Your mark is {correct_answers}/5.')
-
-
-    elif mode == '2':
-        correct_answers = 0
-        for _ in range(5):
-            number = random.randint(11, 29)
-            example = f'{number}**2'
-            print(example)
-            right_answer = number ** 2
-            while True:
-                answer = input('>')
-                if answer.isdigit():
-                    if int(answer) == right_answer:
-                        print('Right!')
-                        correct_answers += 1
-                        break
-                    else:
-                        print('Wrong!')
+                results = input('Would you like to save your result to file? Enter yes or no\n>')
+                if results.lower() in ['yes', 'y']:
+                        u_name = input('Put down your name:\n>')
+                        results_txt = f'{u_name}: your results is {correct_answers}/5.\n'
+                        with open('results.txt', 'a') as file:
+                            file.write(results_txt)
+                        print(results_txt)
                         break
                 else:
-                    print('Incorrect format.')
-            print(f'Your mark is {correct_answers}/5.')
+                    break
 
 
-        results = input('Would you like to save your result to file? Enter yes or no\n>')
-        if results.lower() in ['yes', 'y']:
-            u_name = input('Put down your name:\n>')
-            results_txt = f'{u_name}: your results is {correct_answers}/5.\n'
-            with open('results.txt', 'a') as file:
-                file.write(results_txt)
-            print(results_txt)
-            break
-        else:
-            break
-
-
-    else:
-        print("Invalid mode. Please enter 1 or 2.")
+mode_choosing()
